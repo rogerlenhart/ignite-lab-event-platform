dcd7157b20483a87e1a811527b98485ecdbb00d2
Róger Lenhart <rogerlenhart15@gmail.com>
2022-06-22 17:03:59 -0300

    Layout básico.

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4pv66np2by301xnhf0q89p5/master',
-    cache: new InMemoryCache()
+  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4pv66np2by301xnhf0q89p5/master',
+  cache: new InMemoryCache()
 })
