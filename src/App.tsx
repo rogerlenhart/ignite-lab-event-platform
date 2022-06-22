2cb49e0f6bcfeab8aff7e2fc60d7030778684a4a
Róger Lenhart <rogerlenhart15@gmail.com>
2022-06-22 15:11:11 -0300

    Início do projeto.

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { gql, useQuery } from "@apollo/client"
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+
+      teacher {
+        name
+      }
+    }
+  }
+`
+
+interface Lesson {
+  id: string
+  title: string
+}
+
+function App() {
+
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
+
+  return (
+    <div className="App">
+      <ul>
+        {data?.lessons.map(lesson => {
+          return <li key={lesson.id}>{lesson.title}</li>
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default App
